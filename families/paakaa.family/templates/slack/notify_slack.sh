9816d9e45dee89fae752ee49f8a12cebbeb793c3
Christopher Parker <christopher@parker.jetzt>
2021-10-14 16:31:12 -0400

    Refactor monolithic playbooks making better use of Ansible's functionality.

--- families/paakaa.family/templates/slack/notify_slack.sh
+++ families/paakaa.family/templates/slack/notify_slack.sh
@@ -8,7 +8,7 @@
 # Short-Description: Script used to send notifications to Slack Shutdown/Startup
 ### END INIT INFO
 
-{{ ansible_managed }}
+# {{ ansible_managed }}
 
 # script adapted from http://www.whiteboardcoder.com/2015/07/notify-slack-when-ubuntu-startsstops.html
 
--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 ansible-galaxy install -r ./requirements.yml
 ansible-playbook ./main.yml
-ansible-playbook ./yggdrasil.yml
 # ansible-playbook ./reboot.yml
-
